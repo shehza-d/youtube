b09d788dd1125a660e4dfcf4ff085f23d64206c9
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2023-12-22 23:05:15 +0500

    📂 cloudinary setup

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -2,16 +2,49 @@
 import "dotenv/config";
 
 const PORT = process.env.PORT || 3003;
-const DB_NAME = process.env.DB_NAME || "youtube";
-const DB_USERNAME = process.env.DB_USERNAME || "shehzad";
+const DB_NAME = process.env.DB_NAME || "";
+const DB_USERNAME = process.env.DB_USERNAME || "";
 const DB_PASSWORD = process.env.DB_PASSWORD || "";
 const DB_CLUSTER = process.env.DB_CLUSTER || "";
 const SECRET = process.env.JWT_SECRET || "";
 const CORS_ORIGIN = process.env.CORS_ORIGIN || "*";
+const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET || "";
+const ACCESS_TOKEN_EXPIRY = process.env.ACCESS_TOKEN_EXPIRY || "";
+const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET || "";
+const REFRESH_TOKEN_EXPIRY = process.env.REFRESH_TOKEN_EXPIRY || "";
 
-if (!DB_NAME || !DB_PASSWORD || !DB_USERNAME || !DB_CLUSTER || !SECRET)
+const CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME || "";
+const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY || "";
+const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET || "";
+
+if (
+  !DB_NAME ||
+  !DB_PASSWORD ||
+  !DB_USERNAME ||
+  !DB_CLUSTER ||
+  !SECRET ||
+  !ACCESS_TOKEN_SECRET ||
+  !ACCESS_TOKEN_EXPIRY ||
+  !REFRESH_TOKEN_SECRET ||
+  !REFRESH_TOKEN_EXPIRY
+)
   throw new Error("Missing environment variable in backend/src/config");
 
 const MONGO_URI = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@cluster0.${DB_CLUSTER}.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
 
-export { PORT, DB_NAME, DB_USERNAME, SECRET, MONGO_URI, CORS_ORIGIN };
+export {
+  PORT,
+  DB_NAME,
+  DB_USERNAME,
+  SECRET,
+  MONGO_URI,
+  CORS_ORIGIN,
+  ACCESS_TOKEN_SECRET,
+  ACCESS_TOKEN_EXPIRY,
+  REFRESH_TOKEN_SECRET,
+  REFRESH_TOKEN_EXPIRY,
+  
+  CLOUDINARY_CLOUD_NAME,
+  CLOUDINARY_API_KEY,
+  CLOUDINARY_API_SECRET,
+};
---
+++ backend/src/utils/fileUpload.ts
@@ -0,0 +1,35 @@
+// Cloudinary
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+import {
+  CLOUDINARY_CLOUD_NAME,
+  CLOUDINARY_API_KEY,
+  CLOUDINARY_API_SECRET,
+} from "../config/index.js";
+
+cloudinary.config({
+  cloud_name: CLOUDINARY_CLOUD_NAME,
+  api_key: CLOUDINARY_API_KEY,
+  api_secret: CLOUDINARY_API_SECRET,
+});
+
+const uploadFile = async (localFilePath: string) => {
+  try {
+    if (!localFilePath) return null;
+
+    // upload the file on cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+
+    // file has been uploaded successfully
+    console.log("file is uploaded on cloudinary ", response.url);
+    fs.unlinkSync(localFilePath);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); // remove the locally saved temporary file as the upload operation got failed
+    return null;
+  }
+};
+
+export { uploadFile };

c804328c3a674a646ca6c68ff9415b8ef1174e9d
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2024-01-31 14:53:28 +0500

    💍 app logic improved for hosting

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -1,54 +1,30 @@
-import { MONGO_URI, DB_NAME } from "../config/index.js";
 import mongoose from "mongoose";
-// import { DB_NAME } from "../constants.js";
+import { DB_NAME, MONGO_URI } from "../config/index.js";
 
 const connectDB = async () => {
   try {
-    const connectionInstance = await mongoose.connect(MONGO_URI);
-    console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance}`);
-    console.log(
-      `\n\n 2 MongoDB connected !! DB HOST: ${connectionInstance.connection}`,
+    const connectionInstance = await mongoose.connect(MONGO_URI, {
+      dbName: DB_NAME,
+    });
+
+    console.log(`\n🌿 MongoDB connected ! 🍃\n`);
+
+    mongoose.connection.on(
+      "error",
+      console.error.bind(console, "Connection error:"),
     );
+
+    process.on("SIGINT", () => {
+      // Cleanup code
+      mongoose.connection.close();
+
+      console.log("Mongoose connection closed due to application termination");
+      process.exit(0);
+    });
   } catch (error) {
     console.error("MONGODB connection FAILED ", error);
-    process.exit(1);
+    process.exit(1); // Exited with error
   }
 };
 
 export { connectDB };
-
-// let client: MongoClient;
-// let database: Db;
-
-// try {
-//   client = new MongoClient(MONGO_URI);
-//   database = client.db(DB_NAME);
-// } catch (err) {
-//   console.log("🚀 ~ file: db.ts:16 ~ err:", err);
-// }
-
-// // to know if db connection is successful
-// const testDB = async () => {
-//   try {
-//     await client.connect();
-//     // Send a ping to confirm a successful connection
-//     await client.db(DB_NAME).command({ ping: 1 });
-//     console.log("Pinged your deployment. Successfully connected to MongoDB");
-//   } catch (err) {
-//     console.log("🚀 file:index.mts:26 ~ err:", err);
-//     await client.close();
-//     process.exit(1);
-//   }
-// };
-// testDB();
-
-// process.on("SIGINT", async (code) => {
-//   // Code to run before the server exits
-//   console.log(`Server is terminating with code ${code}`);
-
-//   // Cleanup code
-//   await client.close();
-//   process.exit(0);
-// });
-
-// export { database as db };
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,17 @@
 import { app } from "./app.js";
 import { connectDB } from "./db/index.js";
-import dotenv from "dotenv";
 import { PORT } from "./config/index.js";
-
-dotenv.config({ path: "./env" }); // remove
 
 (async () => {
   try {
     await connectDB();
+
     app.listen(PORT, () =>
-      console.log(`⚙️ Server running at port ==>> ${PORT}`),
+      console.log(`⚙️  Server running at port ==>> ${PORT}`),
     );
-    app.on("error", (error) => console.log("🚀 ~ index.ts:17 ~ error:", error));
+
+    app.on("error", (err) => console.log("🚀 ~ main file:", err));
   } catch (err) {
-    console.log("🚀 ~ file: index.ts:12 ~ err:", err);
+    console.log("🚀 ~ main file ~ err:", err);
   }
 })();
